26cbb7542ee2296a6fd7dd172a96a9c684d08c66
sumayyats123 <sumayyats429@gmail.com>
2024-11-29 12:21:03 +0530

    feat(upload): Initial commit to uload branch

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:assistrends/view/screens/uploadscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) { 
+    return MaterialApp(
+    theme: ThemeData(
+      scaffoldBackgroundColor: const Color(0xFF121212),
+        primaryColor:const Color(0xFF2B4DFC).withOpacity(0.7 ) ,
+                        
+     
+    ),
+
+      debugShowCheckedModeBanner: false,
+      home: const UploadScreen(),
+   
+    );
+  }
+}
